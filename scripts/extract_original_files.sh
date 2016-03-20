c48edc1be7bf80181cee775318715d416f8ca256
cjuub <cjuuub@gmail.com>
2016-03-20 17:21:06 +0100

    Remove unused dialogue files

--- scripts/extract_original_files.sh
+++ scripts/extract_original_files.sh
@@ -7,6 +7,8 @@ mkdir -p ./tmp/out
 ./tools/ndstool -x rom.nds -9 ./tmp/out/arm9.bin -7 ./tmp/out/arm7.bin -y9 ./tmp/out/y9.bin -y7 ./tmp/out/y7.bin -d ./tmp/out/data -y ./tmp/out/overlay -t ./tmp/out/banner.bin -h ./tmp/out/header.bin > /dev/null
 mkdir -p ./tmp/bmg_original
 cp ./tmp/out/data/*.bmg ./tmp/bmg_original
+rm ./tmp/bmg_original/MsgResMory.bmg
+rm ./tmp/bmg_original/MsgResSaka.bmg
 mkdir -p ./tmp/nxxx_original
 cp ./tmp/out/data/2d/*.NCER ./tmp/nxxx_original
 cp ./tmp/out/data/2d/*.NCGR ./tmp/nxxx_original
