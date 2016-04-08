8013879ab8767f299212da04889a2a8a97aadb7b
cjuub <cjuuub@gmail.com>
2016-04-08 12:32:45 +0200

    Translate hardcoded string in overlay_0035

--- scripts/build_translated_nds.sh
+++ scripts/build_translated_nds.sh
@@ -43,6 +43,7 @@ echo " Done!";
 
 echo -n "Patching ARM9 overlays..."
 ./blz -d ./tmp/out/overlay/overlay_0028.bin > /dev/null
+./blz -d ./tmp/out/overlay/overlay_0035.bin > /dev/null
 
 # "What's this?"
 printf '\x57\x68\x61\x74\x27\x73\x20\x74\x68\x69\x73\x3f\x00\x00\x00\x00' | dd of=./tmp/out/overlay/overlay_0028.bin bs=1 seek=70232 count=16 conv=notrunc 2> /dev/null
@@ -55,7 +56,12 @@ printf '\x52\x65\x63\x6f\x72\x64\x73\x00' | dd of=./tmp/out/overlay/overlay_0028
 # "Back"
 printf '\x42\x61\x63\x6b\x00\x00\x00\x00' | dd of=./tmp/out/overlay/overlay_0028.bin bs=1 seek=70284 count=8 conv=notrunc 2> /dev/null
 
+# "*Click*!"
+printf '\x2A\x43\x6C\x69\x63\x6B\x2A\x21\x00\x00\x00\x00' | dd of=./tmp/out/overlay/overlay_0035.bin bs=1 seek=71732 count=12 conv=notrunc 2> /dev/null
+
 ./blz -en ./tmp/out/overlay/overlay_0028.bin > /dev/null
+./blz -en ./tmp/out/overlay/overlay_0035.bin > /dev/null
+
 
 FILE_SIZE=$(stat --printf "%s" ./tmp/out/overlay/overlay_0028.bin)
 FILE_SIZE=$(printf "%x\n" $FILE_SIZE)
